f3df09d484c5deb8bf62ec7f97fb8e128ae02871
Liam Warren <ljwarren@mun.ca>
2021-02-24 15:28:11 -0330

    Revert "Merge branch 'implement-event-system' of https://github.com/Burke-Daniel/Aquarius into implement-event-system"
    This reverts commit 965b89c760b3ea9fa869e6dbaef2a587b6171637, reversing
    changes made to 158f6e131b906634d4589f34d2a1c7ff569d9d1b.

--- Aquarius/Aquarius.cpp
+++ Aquarius/Aquarius.cpp
@@ -1,57 +1,28 @@
 #include "Aquarius.h"
 #include "Aquarius/Core/Log.h"
-#include "Aquarius/Events/Keyboard.h"
-#include "Aquarius/Events/Mouse.h"
-#include "Aquarius/Events/Window.h"
+#include "Aquarius/Core/Window.h"
 
 namespace Aquarius {
 
-int Test::testMain()
-{
-    Log::initLoggers();
-
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Aquarius-Test", NULL, NULL);
-    if (window == NULL)
+    int Test::testMain()
     {
-        AQ_WARNING("Could not create window");
+        Log::initLoggers();
+
+        std::unique_ptr<Window> Window = Window::Create(800, 600, "Aquarius");
+        Window->Initialize();
+
+        AQ_INFO("Testing %v %v", "client", "logger");
+        AQ_CORE_INFO("Testing %v %v", "core", "logger");
+
+        while (1)
+        {
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            Window->OnUpdate();
+        }
+
+        return 0;
     }
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        AQ_WARNING("Could not initialize glad");
-        return -1;
-    }
-
-    AQ_INFO("Window created successfully!");
-
-    //TODO Bring in neccesary set methods for window events.
-    glfwSetKeyCallback(window, KeyboardEvent::Callback);
-    glfwSetCursorPosCallback(window, MouseEvent::cursorPositionCallback);
-    glfwSetCursorEnterCallback(window, MouseEvent::cursorInWindowCallback);
-    glfwSetMouseButtonCallback(window, MouseEvent::mouseButtonCallback);
-    glfwSetScrollCallback(window, MouseEvent::mouseScrollCallback);
-
-    glfwSetInputMode(window, GLFW_STICKY_MOUSE_BUTTONS, 1);
-    glfwSetInputMode(window, GLFW_STICKY_KEYS,1);
-
-    AQ_INFO("Testing %v %v", "client", "logger");
-    AQ_CORE_INFO("Testing %v %v", "core", "logger");
-
-    while (!glfwWindowShouldClose(window))
-    {
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    return 0;
-}
 
 } // namespace Aquarius
+
