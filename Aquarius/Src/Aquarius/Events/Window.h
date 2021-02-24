396ecf200b4545b79b68b1cc7c6e2f0f9f2370d2
Liam Warren <ljwarren@mun.ca>
2021-02-23 21:17:12 -0330

    Initial commit of event system
    Completed
    -Initial Keyboard event callbacks implemented.
    -Initial Mouse event callbacks implemented.
    -Logging completed for all callbacks, can be tested by building and information will appear for all callbacks.
    -Set callbacks in Aquarius.cpp for Keyboard and Mouse completed.
    
    TODO (in order)
    -Implement Window event initial callbacks
    -Set callbacks in Aquarius.cpp for Window.
    -Expand event system
    -Update logging messages to include key and mouse button names rather than their int representation.

---
+++ Aquarius/Src/Aquarius/Events/Window.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+#include "Aquarius/Core/Log.h"
+
+namespace Aquarius {
+    class WindowEvent {
+    public:
+
+        static void windowCloseCallback(GLFWwindow* window)
+        {
+            //TODO Implement
+        }
+
+        static void windowSizeCallback(GLFWwindow* window, int width, int height)
+        {
+            //TODO Implement
+        }
+
+        static void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+        {
+            //TODO Implement
+        }
+
+        static void windowContentScaleCallBack(GLFWwindow* window, float xscale, float yscale)
+        {
+            //TODO Implement
+        }
+
+        static void windowPositionChangedCallback(GLFWwindow* window, int xposition, int yposition)
+        {
+            //TODO Implement
+        }
+
+        static void windowIconifyCallBack(GLFWwindow* window, int iconified)
+        {
+            //TODO Implement
+        }
+
+        static void windowMaximizedCallBack(GLFWwindow* window, int maximized)
+        {
+            //TODO Implement
+        }
+
+        static void windowFocusedCallBack(GLFWwindow*, int focused)
+        {
+            //TODO Implement
+        }
+
+        static void windowRefreshRequiredCallback(GLFWwindow*)
+        {
+            //TODO Implement
+        }
+    };
+}
