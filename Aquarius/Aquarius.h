17146d18b6395c113605467d2fa5c5bc3dd8975d
Colton Smith <cnsmith@mun.ca>
2021-02-25 17:31:32 -0330

    Include all engine headers to Aquarius.h so clients can get access to them for now (except rendering context)

--- Aquarius/Aquarius.h
+++ Aquarius/Aquarius.h
@@ -6,6 +6,22 @@
 #include <stb_image.h>
 #include <glm/glm.hpp>
 
+// -- Core
+#include "Aquarius/Core/Utility.h"
+#include "Aquarius/Core/Application.h"
+#include "Aquarius/Core/Assert.h"
+#include "Aquarius/Core/Input.h"
+#include "Aquarius/Core/Log.h"
+#include "Aquarius/Core/Window.h"
+
+// -- Renderer
+#include "Aquarius/Renderer/IndexBuffer.h"
+#include "Aquarius/Renderer/VertexBuffer.h"
+
+// -- Events
+#include "Aquarius/Events/Keyboard.h"
+#include "Aquarius/Events/Mouse.h"
+#include "Aquarius/Events/Window.h"
 
 namespace Aquarius {
     class Test
