ed4807bd31c97643e515e30d3dac6b196364fd4b
Liam Warren <ljwarren@mun.ca>
2021-02-24 21:27:19 -0330

    Restructuring
    -Renamed files to ensure clarity.
    -Created additional files that will be implemented to refine the initial event system.
    -Commit restructuring changes prior to implementing further changes.

--- Aquarius/Src/Aquarius/Events/KeyboardEvent.h
+++ Aquarius/Src/Aquarius/Events/KeyboardEvent.h
@@ -1,33 +1,37 @@
 #pragma once
 
-#include "Event.h"
-
 #include <GLFW/glfw3.h>
-
+#include "Aquarius/Core/Log.h"
+#include "Aquarius/Events/KeyCode.h"
 
 namespace Aquarius {
 
-    enum class KeyboardEvents
+    class KeyboardEvent
+    {
+    public:
+        static void Callback(GLFWwindow* window, int key, int scanCode, int action, int modifications)
+        {
+            //TODO: Get it so key button name is printed rather then number, this is not critical but would be
+            // nice to have.
+            switch (action)
             {
-        KeyPressedEvent, KeyRepeatedEvent, KeyReleasedEvent,
+                case GLFW_PRESS:
+                    AQ_INFO("key %v is pressed.", key);
+                    break;
+
+                case GLFW_RELEASE:
+                    AQ_INFO("key %v has been released.", key);
+                    break;
+
+                case GLFW_REPEAT:
+                    AQ_INFO("key %v is being pressed repeatedly.", key);
+                    break;
+
+                default:
+                    break;
+            }
+        }
     };
 
-    class KeyPressed : public Event<KeyboardEvents>
-    {
-    public:
-        KeyPressed() : Event<KeyboardEvents>(KeyboardEvents::KeyPressedEvent) {};
-    };
-
-    class KeyRepeated : public Event<KeyboardEvents>
-    {
-    public:
-        KeyRepeated() : Event<KeyboardEvents>(KeyboardEvents::KeyRepeatedEvent) {};
-    };
-
-    class KeyReleased : public Event<KeyboardEvents>
-    {
-    public:
-        KeyReleased() : Event<KeyboardEvents>(KeyboardEvents::KeyReleasedEvent) {};
-    };
 
 } // namespace Aquarius
