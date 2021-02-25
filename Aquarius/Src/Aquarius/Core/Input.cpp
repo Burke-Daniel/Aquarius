a7283dedc04c16d6a3c8360ba43a37e64ad23f01
Daniel Burke <danielseanburke@gmail.com>
2021-02-25 11:42:05 -0330

    Add ability to poll for input from keyboard and mouse, and get the mouse's current position

---
+++ Aquarius/Src/Aquarius/Core/Input.cpp
@@ -0,0 +1,39 @@
+#include "Input.h"
+
+#include "Application.h"
+
+#include <GLFW/glfw3.h>
+
+namespace Aquarius {
+
+	KeyState getKeyState(KeyCode key)
+	{
+		auto window = Application::get()->getWindow();
+		return static_cast<KeyState>(glfwGetKey(window->get(), static_cast<int>(key)));
+	}
+
+	MouseButtonState getMouseButtonState(MouseButtonCode button)
+	{
+		auto window = Application::get()->getWindow();
+		return static_cast<MouseButtonState>(glfwGetMouseButton(window->get(), static_cast<int>(button)));
+	}
+
+	bool isKeyPressed(KeyCode key)
+	{
+		return getKeyState(key) == KeyState::Pressed;
+	}
+
+	bool isMouseButtonPressed(MouseButtonCode button)
+	{
+		return getMouseButtonState(button) == MouseButtonState::Pressed;
+	}
+
+	MousePosition getMousePosition()
+	{
+		auto window = Application::get()->getWindow();
+		MousePosition position;
+		glfwGetCursorPos(window->get(), &position.x, &position.y);
+
+		return position;
+	}
+} // namespace Aquarius
