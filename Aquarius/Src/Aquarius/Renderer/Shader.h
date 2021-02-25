6ea078708fda51e663cca43eb55c09e90950b615
Colton Smith <cnsmith@mun.ca>
2021-02-25 14:16:00 -0330

    Move ShaderType enum into shader class and add map for string conversion

--- Aquarius/Src/Aquarius/Renderer/Shader.h
+++ Aquarius/Src/Aquarius/Renderer/Shader.h
@@ -1,21 +1,16 @@
 #pragma once
+
 #include <iostream>
 #include <glm/glm.hpp>
+#include <unordered_map>
 
 namespace Aquarius {
-
-    enum class ShaderType
-    {
-        VERTEX_SHADER,
-        FRAGMENT_SHADER
-    };
-    std::ostream& operator<< (std::ostream& os, ShaderType shaderType);
-
 
     class Shader
     {
     public:
-        Shader(const std::string& vertexPath );
+        // Path to vertex and fragment shader source code
+        Shader(const std::string& vertexPath, const std::string& fragmentPath);
         ~Shader();
 
         // Enable/disable this shader
@@ -25,15 +20,32 @@ namespace Aquarius {
         // Set uniform methods
         void setBool(const std::string& name, bool value) const;
         void setInt(const std::string& name, int value) const;
+        void setInt2(const std::string& name, const glm::ivec2& values) const;
+        void setInt3(const std::string& name, const glm::ivec3& values) const;
+        void setInt4(const std::string& name, const glm::ivec4& values) const;
         void setFloat(const std::string& name, float value) const;
-        void setMat4(const std::string& name, const glm::mat4& value) const;
+        void setFloat2(const std::string& name, const glm::vec2& values) const;
+        void setFloat3(const std::string& name, const glm::vec3& values) const;
+        void setFloat4(const std::string& name, const glm::vec4& values) const;
+        void setMat4(const std::string& name, bool transpose, const glm::mat4& value) const;
 
+        enum class ShaderType
+        {
+            VERTEX_SHADER,
+            FRAGMENT_SHADER
+        };
+
+        std::unordered_map<ShaderType, std::string> shaderTypeToString =
+        {
+            {ShaderType::VERTEX_SHADER, "VERTEX_SHADER"},
+            {ShaderType::FRAGMENT_SHADER, "FRAGMENT_SHADER"}
+        };
 
     private:
         void compile(uint32_t shaderID, const char* shaderCode, ShaderType shaderType);
+        void link(uint32_t vertexID, uint32_t fragmentID);
         uint32_t m_ID;
     };
-
 } // namespace Aquarius
 
 
