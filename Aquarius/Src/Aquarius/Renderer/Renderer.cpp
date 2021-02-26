d4bc6fb8a667daff6113fd263cd943960bbd3403
Colton Smith <cnsmith@mun.ca>
2021-02-26 15:57:55 -0330

    Add barebones renderer implementation allowing submission of vertex array and shader

---
+++ Aquarius/Src/Aquarius/Renderer/Renderer.cpp
@@ -0,0 +1,36 @@
+#include "Renderer.h"
+#include "Aquarius/Core/Application.h"
+
+#include <glad/glad.h>
+
+
+namespace Aquarius {
+
+    void Renderer::Init()
+    {
+        glPolygonMode(GL_FRONT, GL_FILL);
+    }
+
+    void Renderer::Shutdown()
+    {
+        // Nothing to put here just yet
+    }
+
+    void Renderer::Clear()
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
+
+    void Renderer::ClearColor(glm::vec3 color)
+    {
+        glClearColor(color.x, color.y, color.z, 1.0f);
+    }
+
+    void Renderer::Submit(VertexArray* vertexArray, Shader* shader)
+    {
+        shader->activate();
+        vertexArray->activate();
+        glDrawElements(GL_TRIANGLES, vertexArray->getIndexBuffer()->getCount(), GL_UNSIGNED_INT, (void*)0);
+    }
+
+}
---
+++ Aquarius/Src/Aquarius/Renderer/Renderer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Aquarius/Renderer/VertexArray.h"
+#include "Aquarius/Renderer/IndexBuffer.h"
+#include "Aquarius/Renderer/Shader.h"
+
+#include <glm/glm.hpp>
+
+namespace Aquarius {
+
+    class Renderer
+    {
+    public:
+        Renderer() = delete;
+        ~Renderer() = delete;
+
+        static void Init();
+        static void Shutdown();
+
+        static void Clear();
+        static void ClearColor(glm::vec3 color);
+
+        static void Submit(VertexArray* vertexArray, Shader* shader);
+    };
+
+}
