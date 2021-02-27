40afa517358f2251bf39a11e0cb3a46f3f887be8
Colton Smith <cnsmith@mun.ca>
2021-02-27 17:35:33 -0330

    Add quad rendering info struct to Renderer.cpp

--- Aquarius/Src/Aquarius/Renderer/Renderer.cpp
+++ Aquarius/Src/Aquarius/Renderer/Renderer.cpp
@@ -1,16 +1,58 @@
 #include "Renderer.h"
 #include "Aquarius/Core/Application.h"
+#include "Aquarius/Renderer/VertexArray.h"
 
+#include <cstdint>
 #include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <vector>
 
 
 namespace Aquarius {
 
         namespace Renderer {
 
+            struct quadRendererInfo
+            {
+                sharedPtr<VertexArray> vertexArray;
+                sharedPtr<Shader> shader;
+                glm::vec4 defaultColor = {1,1,1,1};
+            };
+
+            static SceneInfo s_SceneData =
+            {
+                    glm::mat4(1.0f),
+                    glm::mat4(1.0f)
+            };
+
+            static quadRendererInfo s_QuadData;
+
             void Init()
             {
-                glPolygonMode(GL_FRONT, GL_FILL);
+                std::vector<float> quadVertices =
+                {
+                        0.0f, 0.0f, // Bottom left
+                        1.0f, 0.0f, // Bottom right
+                        1.0f, 1.0f, // Top right
+                        0.0f, 1.0f  // Top left
+                };
+
+                std::vector<uint32_t> quadIndices =
+                {
+                        0, 1, 2,
+                        2, 3, 0
+                };
+
+                // TODO - gross
+                auto vertexBuffer = std::make_shared<VertexBuffer>(quadVertices.data(),quadVertices.size() * sizeof(quadVertices[0]));
+                auto indexBuffer = std::make_shared<IndexBuffer>(quadIndices.data(), quadIndices.size());
+                auto bufferLayout = BufferLayout({
+                    VertexElement(ShaderType::Float, 2, false)
+                });
+
+                s_QuadData.vertexArray = std::make_shared<VertexArray>(vertexBuffer, indexBuffer, bufferLayout);
+                s_QuadData.shader = std::make_unique<Shader>("Shaders/vertexShader.glsl", "Shaders/fragmentShader.glsl");
             }
 
             void Shutdown()
