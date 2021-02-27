2b2a4c08f7ccc960f2ca1151133f81f902de3943
Colton Smith <cnsmith@mun.ca>
2021-02-27 17:34:09 -0330

    Add transformation matrices to the sandbox shaders

--- Sandbox/Shaders/defaultVertexShader.glsl
+++ Sandbox/Shaders/defaultVertexShader.glsl
@@ -2,12 +2,12 @@
 
 // Basic Vertex Shader
 layout (location = 0) in vec2 aPos;
-layout (location = 1) in vec3 aColor;
 
-out vec3 fColor;
+uniform mat4 u_model;
+uniform mat4 u_view;
+uniform mat4 u_projection;
 
 void main()
 {
-   gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);
-   fColor = aColor;
+   gl_Position = u_projection * u_view * u_model * (aPos.x, aPos.y, 0.0, 1.0);
 }
