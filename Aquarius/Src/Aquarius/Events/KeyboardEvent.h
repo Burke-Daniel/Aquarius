4738562f0d309621846bf71684f7af858664dae7
Liam Warren <ljwarren@mun.ca>
2021-02-27 21:24:26 -0330

    Reverted to modifications

--- Aquarius/Src/Aquarius/Events/KeyboardEvent.h
+++ Aquarius/Src/Aquarius/Events/KeyboardEvent.h
@@ -1,36 +1,33 @@
 #pragma once
 
+#include "Event.h"
+
 #include <GLFW/glfw3.h>
-#include "Aquarius/Core/Log.h"
+
 
 namespace Aquarius {
 
-    class KeyboardEvent
+    enum class KeyboardEvents
     {
-    public:
-        static void Callback(GLFWwindow* window, int key, int scanCode, int action, int modifications)
-        {
-            //TODO: Get it so key button name is printed rather then number, this is not critical but would be
-            // nice to have.
-            switch (action)
-            {
-                case GLFW_PRESS:
-                    AQ_INFO("key %v is pressed.", key);
-                    break;
-
-                case GLFW_RELEASE:
-                    AQ_INFO("key %v has been released.", key);
-                    break;
-
-                case GLFW_REPEAT:
-                    AQ_INFO("key %v is being pressed repeatedly.", key);
-                    break;
-
-                default:
-                    break;
-            }
-        }
+        KeyPressedEvent, KeyRepeatedEvent, KeyReleasedEvent,
     };
 
+    class KeyPressed : public Event<KeyboardEvents>
+    {
+    public:
+        KeyPressed() : Event<KeyboardEvents>(KeyboardEvents::KeyPressedEvent) {};
+    };
+
+    class KeyRepeated : public Event<KeyboardEvents>
+    {
+    public:
+        KeyRepeated() : Event<KeyboardEvents>(KeyboardEvents::KeyRepeatedEvent) {};
+    };
+
+    class KeyReleased : public Event<KeyboardEvents>
+    {
+    public:
+        KeyReleased() : Event<KeyboardEvents>(KeyboardEvents::KeyReleasedEvent) {};
+    };
 
 } // namespace Aquarius
--- Aquarius/Src/Aquarius/Events/MouseEvent.h
+++ Aquarius/Src/Aquarius/Events/MouseEvent.h
@@ -1,57 +1,45 @@
 #pragma once
 
+#include "Event.h"
+
 #include <GLFW/glfw3.h>
-#include "Aquarius/Core/Log.h"
+
 
 namespace Aquarius {
 
-    class MouseEvent
+    enum class MouseEvents
+    {
+        MouseButtonPressedEvent, MouseButtonReleasedEvent, MouseMovedEvent, MouseScrolledEvent, MouseInWindowEvent
+    };
+
+    class MouseButtonPressed : public Event<MouseEvents>
     {
     public:
-        static void cursorPositionCallback(GLFWwindow* window, double xPosition, double yPosition)
-        {
-            // TODO: Discuss if we want to only keep track of cursor position when the cursor is on
-            //  the Application window, if so can combine this callback method and the one below.
-            AQ_INFO("x position: %v", xPosition);
-            AQ_INFO("y position: %v", yPosition);
-        }
-
-        static void cursorInWindowCallback(GLFWwindow* window, int inWindow)
-        {
-            if (inWindow)
-            {
-                AQ_INFO("Cursor has entered application window.");
-            }
-
-            else
-            {
-                AQ_INFO("Cursor has left application window.");
-            }
-        }
-
-        static void mouseButtonCallback(GLFWwindow* window, int button, int action, int modifications)
-        {
-            switch (action)
-            {
-                //TODO: Get it so mouse button name is printed rather then number, this is not critical but would be
-                // nice to have.
-                case GLFW_PRESS:
-                    AQ_INFO("mouse button %v is pressed.", button);
-                    break;
-
-                case GLFW_RELEASE:
-                    AQ_INFO("mouse button %v has been released.", button);
-                    break;
-
-                default:
-                    break;
-            }
-        }
-
-        static void mouseScrollCallback(GLFWwindow* window, double xOffSet, double yOffset)
-        {
-            AQ_INFO("x offset: %v", xOffSet);
-            AQ_INFO("y offset: %v", yOffset);
-        }
+        MouseButtonPressed() : Event<MouseEvents>(MouseEvents::MouseButtonPressedEvent) {};
     };
+
+    class MouseButtonReleased : public Event<MouseEvents>
+    {
+    public:
+        MouseButtonReleased() : Event<MouseEvents>(MouseEvents::MouseButtonReleasedEvent) {};
+    };
+
+    class MouseMoved : public Event<MouseEvents>
+    {
+    public:
+        MouseMoved() : Event<MouseEvents>(MouseEvents::MouseMovedEvent) {};
+    };
+
+    class MouseScrolled : public Event<MouseEvents>
+    {
+    public:
+        MouseScrolled() : Event<MouseEvents>(MouseEvents::MouseScrolledEvent) {};
+    };
+
+    class MouseInWindow : public Event<MouseEvents>
+    {
+    public:
+        MouseInWindow() : Event<MouseEvents>(MouseEvents::MouseInWindowEvent) {};
+    };
+
 } // namespace Aquarius
