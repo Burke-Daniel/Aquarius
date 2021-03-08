0045c3d497d9afb9e363f57b3272cf5bfee554e6
Daniel Burke <danielseanburke@gmail.com>
2021-03-08 20:07:50 -0330

    Add destuctor which calls glDeleteTextures, and use RGBA8/RGB8 for internal format in glTexImage2D call

--- Aquarius/Src/Aquarius/Renderer/Texture.cpp
+++ Aquarius/Src/Aquarius/Renderer/Texture.cpp
@@ -30,9 +30,10 @@ namespace Aquarius {
 		
 		if (data != nullptr)
 		{
+			uint32_t internalFormat = hasAlpha ? GL_RGBA8 : GL_RGB8;
 			uint32_t format = hasAlpha ? GL_RGBA : GL_RGB;
 
-			glTexImage2D(GL_TEXTURE_2D, 0, format, m_Width, m_Height, 0, format, GL_UNSIGNED_BYTE, data);
+			glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, m_Width, m_Height, 0, format, GL_UNSIGNED_BYTE, data);
 			glGenerateMipmap(GL_TEXTURE_2D);
 			
 			success = true;
