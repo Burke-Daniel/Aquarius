0904c8750a79b9719f6f9450ab80774e1a6ba40c
Colton Smith <cnsmith@mun.ca>
2021-03-08 22:39:10 -0330

    Move implementation of ortho camera to .cpp file and add zoom functionality

--- Aquarius/Src/Aquarius/Renderer/OrthographicCamera.cpp
+++ Aquarius/Src/Aquarius/Renderer/OrthographicCamera.cpp
@@ -2,9 +2,82 @@
 
 namespace Aquarius {
 
+	OrthographicCamera::OrthographicCamera(float moveSpeed, float zoomSpeed, float height, float width)
+		: m_Up{ 0, 1, 0 }, 
+		  m_Position{ 0, 0, -1 }, 
+		  m_cameraFront{ 0, 0, -1 },
+		  m_moveSpeed(moveSpeed),
+		  m_Zoom(1),
+		  m_ZoomSpeed(zoomSpeed),
+		  m_Height(height),
+		  m_Width(width)
+	{
+		updateView();
+		updateProjection();
+	}
+
+	void OrthographicCamera::onUpdate(timeDelta_t dt)
+	{
+		// Delta position 
+		float dx = dt * m_moveSpeed;
+		float dzoom = dt * 0.1 * m_ZoomSpeed;
+
+		// Up
+		if (Input::isKeyPressed(Input::KeyCode::Key_w))
+		{
+			m_Position.y -= dx;;
+		}
+
+		// Down
+		if (Input::isKeyPressed(Input::KeyCode::Key_s))
+		{
+			m_Position.y += dx;
+		}
+
+		// Left
+		if (Input::isKeyPressed(Input::KeyCode::Key_a))
+		{
+			m_Position.x -= dx;
+		}
+
+		// Right
+		if (Input::isKeyPressed(Input::KeyCode::Key_d))
+		{
+			m_Position.x += dx;
+		}
+
+		// TODO - figure out zoom (this should scale the view matrix ?) 
+		if (Input::isKeyPressed(Input::KeyCode::Key_down))
+		{
+			m_Zoom = m_Zoom > 0.1 ? m_Zoom - dzoom : m_Zoom;
+		}
+
+		if (Input::isKeyPressed(Input::KeyCode::Key_up))
+		{
+			m_Zoom += dzoom;
+		}
 
 
+		updateView();
+		updateProjection();
+	}
 
+	void OrthographicCamera::updateView()
+	{
+		// Eye, center, up
+		m_View = glm::lookAt(m_Position,
+							 m_Position + m_cameraFront,
+							 m_Up);
 
+		// Shift to center of screen, then to camera location, then scale, and finally translate back to origin
+		m_View = glm::translate(m_View, { m_Width / 2.0f + m_Position.x, m_Height / 2.0f + m_Position.y, 0.0f });
+		m_View = glm::scale(m_View, { m_Zoom, m_Zoom, 1 });
+		m_View = glm::translate(m_View, { -(m_Width / 2.0f + m_Position.x), -(m_Height / 2.0f + m_Position.y), 0.0f });
+	}
 
-}
+	void OrthographicCamera::updateProjection()
+	{
+		m_Projection = glm::ortho(0.0f, m_Width, m_Height, 0.0f, -1.0f, 1.0f);
+	}
+
+} // namespace Aquarius
--- Aquarius/Src/Aquarius/Renderer/OrthographicCamera.h
+++ Aquarius/Src/Aquarius/Renderer/OrthographicCamera.h
@@ -13,88 +13,19 @@ namespace Aquarius {
 	class OrthographicCamera
 	{
 	public:
-		OrthographicCamera(float moveSpeed, float left, float right, float bot, float top)
-		{
-			m_Position = glm::vec3(0.0f, 0.0f, 1.0f);
-			m_Up = glm::vec3(0.0f, 1.0f, 0.0f);
-			m_moveSpeed = moveSpeed;
-			m_cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-			
-			updateView();
-			updateProjection(left, right, bot, top);
-		}
-
+		OrthographicCamera(float moveSpeed, float zoomSpeed, float height, float width);
 		~OrthographicCamera() = default;
 
-		void onUpdate(timeDelta_t dt)
-		{
-			// Delta pos = time * speed (no accel yet)
-			float dx = dt * m_moveSpeed;
+		void onUpdate(timeDelta_t dt);
+		void updateView();
+		void updateProjection();
 
-			// TODO - Why
-			// Up
-			if (Input::isKeyPressed(Input::KeyCode::Key_w))
-			{
-				m_Position.y -= dx;;
-			}
-
-			// Down
-			if (Input::isKeyPressed(Input::KeyCode::Key_s))
-			{
-				m_Position.y += dx;
-			}
-
-			// Left
-			if (Input::isKeyPressed(Input::KeyCode::Key_a))
-			{
-				m_Position.x -= dx;
-			}
-
-			// Right
-			if (Input::isKeyPressed(Input::KeyCode::Key_d))
-			{
-				m_Position.x += dx;
-			}
-
-			// TODO - figure out zoom
-			if (Input::isKeyPressed(Input::KeyCode::Key_down))
-			{
-				m_Position.z += dx;
-			}
-
-			if (Input::isKeyPressed(Input::KeyCode::Key_up))
-			{
-				m_Position.z -= dx;
-
-			}
-
-			updateView();
-		}
-
-		// TODO - Recalculate projection on window resize
-		// TODO - Recalculate view on scroll (slide camera back along the z axis)
-		void onEvent(); 
-
-
-		void updateView() 
-		{ 
-			// Eye, center, up
-			m_View = glm::lookAt(m_Position, 
-								 m_Position + m_cameraFront,
-								 m_Up);
-		}
-
-		
-		void updateProjection(float left, float right, float bottom, float top, float nearClip=-1.0f, float farClip=1.0f)
-		{
-			m_Projection = glm::ortho(left, right, bottom, top, nearClip, farClip);
-		}
-
+		void setPosition(const glm::vec3& pos) { m_Position = pos; updateView(); }
+		void setZoom(float zoom) { m_Zoom = zoom; updateView(); }
+		float getZoom() const { return m_Zoom; }
 		const glm::mat4& getView() const { return m_View; };
 		const glm::mat4& getProjection() const { return m_Projection; }
 		const glm::vec3& getPosition() const { return m_Position; }
-		void setPosition(const glm::vec3& pos) { m_Position = pos; updateView(); }
-
 	private:
 		glm::vec3 m_Up;
 		glm::vec3 m_Position;
@@ -102,6 +33,10 @@ namespace Aquarius {
 		glm::mat4 m_View;
 		glm::mat4 m_Projection;
 		float m_moveSpeed;
+		float m_Zoom;
+		float m_ZoomSpeed;
+		float m_Height;
+		float m_Width;
 	};
 
 } // namespace Aquarius
