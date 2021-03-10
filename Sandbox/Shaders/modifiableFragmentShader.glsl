30355fad2b4e2cd8d285c2fd64668a24a342a03d
Colton Smith <cnsmith@mun.ca>
2021-03-10 14:39:29 -0330

    Fix sandbox frag shader having merge error text

--- Sandbox/Shaders/modifiableFragmentShader.glsl
+++ Sandbox/Shaders/modifiableFragmentShader.glsl
@@ -10,8 +10,4 @@ uniform sampler2D fTexture;
 void main()
 {
      FragColor = texture(fTexture, fTexCoord) * vec4(fColor, 1.0); 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 01dbdf6 (Remove default shaders from Sandbox and place them in Src/Renderer, add back sandbox shader that got merc'd by rebase somehow)
