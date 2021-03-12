25aef32855ccef04824bc7892d72efb8b54cfaa2
Colton Smith <cnsmith@mun.ca>
2021-03-12 01:03:35 -0330

    Update quad renderer shaders to accept sampler2D uniform and texCoords

--- Aquarius/Src/Aquarius/Renderer/Shaders/vertexShader.glsl
+++ Aquarius/Src/Aquarius/Renderer/Shaders/vertexShader.glsl
@@ -2,6 +2,9 @@
 
 // Basic Vertex Shader
 layout (location = 0) in vec2 aPos;
+layout (location = 1) in vec2 aTexCoord;
+
+out vec2 fTexCoord;
 
 uniform mat4 u_model;
 uniform mat4 u_view;
@@ -10,4 +13,5 @@ uniform mat4 u_projection;
 void main()
 {
    gl_Position = u_projection * u_view * u_model * vec4(aPos.x, aPos.y, 0.0, 1.0);
+   fTexCoord = aTexCoord; 
 }
