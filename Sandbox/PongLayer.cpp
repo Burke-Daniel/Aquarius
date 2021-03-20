4ef6c53bd5969eefcac8021ef578d66a170cba19
Daniel Burke <danielseanburke@gmail.com>
2021-03-20 13:14:43 -0230

    Add collision with paddles

--- Sandbox/PongLayer.cpp
+++ Sandbox/PongLayer.cpp
@@ -2,7 +2,19 @@
 
 #include "Aquarius/Renderer/Renderer.h"
 
-void Paddle::Render()
+#include <array>
+#include <cmath>
+
+static constexpr auto bottomLeft = 0;
+static constexpr auto bottomRight = 1;
+static constexpr auto topRight = 2;
+static constexpr auto topLeft = 3;
+static constexpr auto x = 0;
+static constexpr auto y = 1;
+static constexpr auto pi = 3.14159265358979323846;
+
+
+void Paddle::Render() const 
 {
 	Aquarius::Renderer::DrawQuad(
 		position,
@@ -30,23 +42,83 @@ void Paddle::moveDown(Aquarius::timeDelta_t dt)
 	}
 }
 
-PongLayer::PongLayer()
-	: Layer("Pong"), m_LeftPaddle(), m_RightPaddle()
-{}
+void Ball::Render() const
+{
+	Aquarius::Renderer::DrawQuad(
+		position,
+		size,
+		0,
+		{ 1.0, 0.5, 1.0, 1.0 }
+	);
+}
 
-void PongLayer::onCreation()
+void Ball::Update(Aquarius::timeDelta_t dt)
 {
 	Aquarius::Application* app = Aquarius::Application::get();
 	Aquarius::Window* window = app->getWindow();
 
 	int height = window->getHeight();
 	int width = window->getWidth();
+	auto dx = dt * velocity.x;
+	auto dy = dt * velocity.y;
+	
+	position.x += dx;
+	position.y += dy;
+
+	if (position.y <= 0 || position.y >= window->getHeight() - 10.0)
+	{
+		position.y -= dy;
+		velocity.y = -velocity.y;
+	}
+
+	if (position.x <= 0)
+	{
+		AQ_INFO("Right Paddle Scored!");
+		Reset(false);
+	}
+
+	if (position.x >= window->getWidth() + 10.0)
+	{
+		AQ_INFO("Left Paddle Scored!");
+		Reset(true);
+	}
+}
+
+void Ball::Reset(bool leftScored)
+{
+	static float yVelocity = 0.2;
+	Aquarius::Application* app = Aquarius::Application::get();
+	Aquarius::Window* window = app->getWindow();
+
+	yVelocity = -yVelocity;
+	// TODO Randomize starting velocity
+	position = { (window->getWidth() / 2.0) - 5.0, (window->getHeight() / 2.0) - 5.0 };
+	velocity = { leftScored ? 0.2 : -0.2, yVelocity };
+}
+
+PongLayer::PongLayer()
+	: Layer("Pong"), m_LeftPaddle(), m_RightPaddle()
+{}
+
+void PongLayer::onCreation()
+{
+	// TODO just get a GD member variable to point to application
+
+
+	int height = window.getHeight();
+	int width = window.getWidth();
 
 	m_Camera = std::make_shared<Aquarius::OrthographicCamera>(1, 0.01, height, width);
 	Aquarius::Renderer::Init();
 
-	m_LeftPaddle = { { 20.0, (window->getHeight() / 2.0) - 40.0}, { 10.0, 80.0 }, 1.0, };
-	m_RightPaddle = { { window->getWidth() - 30.0, (window->getHeight() / 2.0) - 40.0 }, { 10.0, 80.0 }, 1.0 };
+	m_LeftPaddle = { { 20.0, (window.getHeight() / 2.0) - 40.0}, { 10.0, 80.0 }, 0.3 };
+	m_RightPaddle = { { window.getWidth() - 30.0, (window.getHeight() / 2.0) - 40.0 }, { 10.0, 80.0 }, 0.3 };
+	m_Ball = {
+		{ (window.getWidth() / 2.0) - 5.0, (window.getHeight() / 2.0) - 5.0 },
+		{ 10, 10 },
+		{ 0.2, 0.2 },
+		{ -0.2, 0.0 }
+	};
 }
 
 void PongLayer::onEvent(const Aquarius::Event& event)
@@ -84,13 +156,82 @@ void PongLayer::onUpdate(Aquarius::timeDelta_t dt)
 		m_RightPaddle.moveDown(dt);
 	}
 
+	m_Ball.Update(dt);
+	checkPaddleCollision();
+
 	m_LeftPaddle.Render();
 	m_RightPaddle.Render();
+	m_Ball.Render();
+}
 
-	Aquarius::Renderer::DrawQuad(
-		{ (window->getWidth() / 2.0) - 5.0, (window->getHeight() / 2.0) - 5.0 },
-		{ 10, 10 },
-		0,
-		{ 1.0, 0.5, 1.0, 1.0 });
+void PongLayer::checkPaddleCollision()
+{
+	// Starting at bottom left, going counter-clockwise
+	float leftPaddleCoords[4][2] = {
+		{ m_LeftPaddle.position.x, m_LeftPaddle.position.y + m_LeftPaddle.size.y },
+		{ m_LeftPaddle.position.x + m_LeftPaddle.size.x, m_LeftPaddle.position.y + m_LeftPaddle.size.y },
+		{ m_LeftPaddle.position.x + m_LeftPaddle.size.x, m_LeftPaddle.position.y },
+		{ m_LeftPaddle.position.x, m_LeftPaddle.position.y}
+	};
 
+	float rightPaddleCoords[4][2] = {
+		{ m_RightPaddle.position.x, m_RightPaddle.position.y + m_RightPaddle.size.y },
+		{ m_RightPaddle.position.x + m_RightPaddle.size.x, m_RightPaddle.position.y + m_RightPaddle.size.y },
+		{ m_RightPaddle.position.x + m_RightPaddle.size.x, m_RightPaddle.position.y },
+		{ m_RightPaddle.position.x, m_RightPaddle.position.y}
+	};
+
+	float ballCoords[4][2] = {
+		{ m_Ball.position.x, m_Ball.position.y + m_Ball.size.y },
+		{ m_Ball.position.x + m_Ball.size.x, m_Ball.position.y + m_Ball.size.y },
+		{ m_Ball.position.x + m_Ball.size.x, m_Ball.position.y },
+		{ m_Ball.position.x, m_Ball.position.y}
+	};
+
+	// Check Collision with right paddle
+
+	if (ballCoords[topRight][x] >= rightPaddleCoords[topLeft][x])
+	{
+		if (ballCoords[topRight][y] >= rightPaddleCoords[topLeft][y] &&
+			ballCoords[topRight][y] <= rightPaddleCoords[bottomLeft][y])
+		{
+			AQ_INFO("Collision with right paddle!!");
+			handleCollision(false);
+		}
+	}
+
+	if (ballCoords[topLeft][x] <= leftPaddleCoords[topRight][x])
+	{
+		if (ballCoords[topLeft][y] >= leftPaddleCoords[topRight][y] &&
+			ballCoords[topLeft][y] <= leftPaddleCoords[bottomRight][y])
+		{
+			AQ_INFO("Collision with left paddle!!");
+			handleCollision(true);
+		}
+	}
+}
+
+void PongLayer::handleCollision(bool isLeftPaddle)
+{
+	if (isLeftPaddle)
+	{
+		auto relativeIntersectY = (m_LeftPaddle.position.y + m_LeftPaddle.size.y / 2.0) - m_Ball.position.y;
+		auto normalizedIntersectY = relativeIntersectY / (m_LeftPaddle.size.y / 2.0);
+		
+		// TODO make departure angle configurable
+		auto bounceAngle = normalizedIntersectY * 45.0;
+		m_Ball.velocity = { m_Ball.speed.x * cos(bounceAngle * (pi / 180.0)), -m_Ball.speed.y * sin(bounceAngle * (pi / 180.0)) };
+		m_Ball.speed = { m_Ball.speed.x + 0.02, m_Ball.speed.y };
+	}
+
+	else
+	{
+		auto relativeIntersectY = (m_RightPaddle.position.y + m_RightPaddle.size.y / 2.0) - m_Ball.position.y;
+		auto normalizedIntersectY = relativeIntersectY / (m_RightPaddle.size.y / 2.0);
+
+		// TODO make departure angle configurable
+		auto bounceAngle = normalizedIntersectY * 75.0;
+		m_Ball.velocity = { -m_Ball.speed.x * cos(bounceAngle * (pi / 180.0)), -m_Ball.speed.y * sin(bounceAngle * (pi / 180.0)) };
+		m_Ball.speed = { m_Ball.speed.x + 0.02, m_Ball.speed.y };
+	}
 }
--- Sandbox/PongLayer.h
+++ Sandbox/PongLayer.h
@@ -5,12 +5,30 @@
 
 struct Paddle
 {
-	void Render();
+	void Render() const;
 	void moveUp(Aquarius::timeDelta_t dt);
 	void moveDown(Aquarius::timeDelta_t dt);
 	glm::vec2 position;
 	glm::vec2 size;
 	double speedY;
+};
+
+struct Score
+{
+	uint8_t RightScore;
+	uint8_t LeftScore;
+};
+
+struct Ball
+{
+	void Render() const;
+	void Update(Aquarius::timeDelta_t dt);
+	void Reset(bool leftScored);
+	
+	glm::vec2 position;
+	glm::vec2 size;
+	glm::vec2 speed;
+	glm::vec2 velocity;
 };
 
 class PongLayer : public Aquarius::Layer
@@ -23,7 +41,16 @@ public:
 	void onUpdate(Aquarius::timeDelta_t) override;
 
 private:
+	void checkPaddleCollision();
+	void handleCollision(bool isLeftPaddle);
+
+	// Helper references
+	Aquarius::Application& app = *Aquarius::Application::get();
+	Aquarius::Window& window = *app.getWindow();
+	 
 	Aquarius::sharedPtr<Aquarius::OrthographicCamera> m_Camera;
 	Paddle m_LeftPaddle;
 	Paddle m_RightPaddle;
+	Ball m_Ball;
+	Score score = { 0, 0 };
 };
