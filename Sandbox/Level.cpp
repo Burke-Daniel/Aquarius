4fe0bbf3e6590b42217f9dc0cb17352cd261516b
Colton Smith <cnsmith@mun.ca>
2021-03-22 21:49:07 -0230

    Implement level drawing and draw a level

--- Sandbox/Level.cpp
+++ Sandbox/Level.cpp
@@ -1,17 +1,72 @@
 #include "Level.h"
 
-Level::Level()
+
+Level::Level(Aquarius::SpriteSheet* spritesheet,
+			 uint32_t screenWidth,
+			 uint32_t screenHeight,
+			 uint32_t tileWidth,
+			 uint32_t tileHeight,
+			 const std::string& name)
 {
+	// Height and width of the screen in number of textures 
+	m_name = name;
+	m_spritesheet = spritesheet;
+	m_tileHeight = tileHeight;
+	m_tileWidth = tileWidth;
+	m_height = std::ceil( (float)screenHeight / tileHeight);
+	m_width = std::ceil((float)screenWidth / tileWidth);
+	m_map = std::vector<std::vector<Tile>>(m_height, std::vector<Tile>(m_width));
+
+	auto texCoords = Aquarius::QuadTexCoords();
+
+	for (int i = 0; i < m_height; i++)
+	{
+		for (int j = 0; j < m_width; j++)
+		{
+			float x = j * m_tileWidth;
+			float y = i * m_tileHeight;
+			m_map[i][j] = { 0, 0, x, y, texCoords, true };
+		}
+	}
 }
 
-void Level::Draw()
+void Level::setTile(int mapI, int mapJ, int spriteI, int spriteJ)
 {
+	m_map[mapI][mapJ].i = spriteI;
+	m_map[mapI][mapJ].j = spriteJ;
+	Aquarius::QuadTexCoords texCoords = m_spritesheet->getSpriteCoords(spriteI, spriteJ);
+	m_map[mapI][mapJ].texCoords = texCoords;
+	m_map[mapI][mapJ].clear = false;
 }
 
-void Level::Serialize()
+void Level::clearTile(int mapI, int mapJ)
 {
+	m_map[mapI][mapJ].i = 0;
+	m_map[mapI][mapJ].j = 0;
+	auto texCoords = Aquarius::QuadTexCoords();
+	m_map[mapI][mapJ].texCoords = texCoords;
+	m_map[mapI][mapJ].clear = true;
 }
 
-void Level::Load()
+void Level::draw()
 {
+	// Draw each of the map tiles
+	for (int i = 0; i < m_height; i++)
+	{
+		for (int j = 0; j < m_width; j++)
+		{
+			Aquarius::Renderer::DrawQuad(
+				{m_map[i][j].x, m_map[i][j].y},
+				{m_tileWidth, m_tileHeight},
+				m_spritesheet->getTexture(),
+				&m_map[i][j].texCoords,
+				0.0f
+			);
+		}
+	}
+}
+
+void Level::serialize()
+{
+
 }
--- Sandbox/Level.h
+++ Sandbox/Level.h
@@ -6,12 +6,29 @@
 class Level
 {
 public:
-	Level();
+	struct Tile;
+	Level(Aquarius::SpriteSheet* spritesheet, 
+		  uint32_t screenWidth, 
+		  uint32_t screenHeight, 
+		  uint32_t tileWidth,
+		  uint32_t tileHeight,
+		  const std::string& name
+		);
+
 	~Level() = default;
 
-	void Draw();
-	void Serialize();
-	void Load();
+	void setTile(int mapI, int mapJ, int spriteI, int spriteJ);
+
+	void clearTile(int mapI, int mapJ);
+
+	// Draw each of the textured quads in the world
+	void draw();
+
+	// Save the map to a file
+	void serialize();
+
+	int getHeight() const { return m_height; };
+	int getWidth() const { return m_width; };
 
 	struct Tile
 	{
@@ -22,12 +39,22 @@ public:
 		// World Coordinates
 		float x; 
 		float y;
+
+		// Texture coords
+		Aquarius::QuadTexCoords texCoords;
+
+		bool clear;
 	};
 
 private:
 	std::string m_name;
+	Aquarius::SpriteSheet* m_spritesheet;
 	int m_height;
 	int m_width;
+	int m_tileHeight;
+	int m_tileWidth;
+
+	// TODO Could be an array
 	std::vector<std::vector<Tile>> m_map;
 };
 
