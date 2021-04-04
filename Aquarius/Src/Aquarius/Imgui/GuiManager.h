a5d1d193b7e9e75970756d8042966f15ae217727
Colton Smith <cnsmith@mun.ca>
2021-04-04 18:34:49 -0230

    Implement imgui manager based on provided example

--- Aquarius/Src/Aquarius/Imgui/GuiManager.h
+++ Aquarius/Src/Aquarius/Imgui/GuiManager.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
@@ -7,6 +9,83 @@ namespace Aquarius {
 	
 	namespace Gui {
 
+        enum class ImguiStyle
+        {
+            DARK,
+            LIGHT,
+            CLASSIC
+        };
+
+        static void Init()
+        {
+            ImGui::CreateContext();
+            ImGuiIO& io = ImGui::GetIO();
+
+            // Enable Keyboard Controls
+            io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     
+        }
+
+        // Setup Platform/Renderer backends
+        static void AttachRenderer(GLFWwindow* window, const std::string& glslVersion)
+        {
+            ImGui_ImplGlfw_InitForOpenGL(window, true);
+            ImGui_ImplOpenGL3_Init(glslVersion.c_str());
+        }
+
+        // Set the style as either classic, dark, or light
+        static void SetStyle(ImguiStyle style)
+        {
+            switch (style)
+            {
+            case ImguiStyle::DARK:
+                ImGui::StyleColorsDark();
+                break;
+            case ImguiStyle::LIGHT:
+                ImGui::StyleColorsLight();
+                break;
+            case ImguiStyle::CLASSIC:
+                ImGui::StyleColorsClassic();
+                break;
+            default:
+                ImGui::StyleColorsDark();
+            }
+        }
+
+        // Begin the ImGUI frame
+        static void NewFrame()
+        {
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+        }
+
+        // End frame / Render all ImGUI draw data
+        static void Render()
+        {
+            ImGuiIO& io = ImGui::GetIO();
+
+            // Set the ImGUI display size to the current window width and height
+            Application* app = Application::get();
+            io.DisplaySize = ImVec2(app->getWindow()->getWidth(), app->getWindow()->getHeight());
+
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        }
+
+        // Free all ImGUI resources 
+        static void Shutdown()
+        {
+            ImGui_ImplOpenGL3_Shutdown();
+            ImGui_ImplGlfw_Shutdown();
+            ImGui::DestroyContext();
+        }
+
+        // Display ImGUI Demo Window
+        static void ShowDemoWindow(bool* show)
+        {
+            ImGui::ShowDemoWindow(show);
+        }
 
 	} // namespace Gui
 
