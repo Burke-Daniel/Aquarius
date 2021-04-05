0b167ea534027fdd64b8eef851b088e8a3866296
Colton Smith <cnsmith@mun.ca>
2021-04-04 23:15:52 -0230

    Remove scuffed static functions and define them in the header

--- Aquarius/Src/Aquarius/Imgui/GuiManager.h
+++ Aquarius/Src/Aquarius/Imgui/GuiManager.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include "Aquarius/Core/Application.h"
+
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
+#include <string>
 
 
 namespace Aquarius {
@@ -16,76 +19,25 @@ namespace Aquarius {
             CLASSIC
         };
 
-        static void Init()
-        {
-            ImGui::CreateContext();
-            ImGuiIO& io = ImGui::GetIO();
-
-            // Enable Keyboard Controls
-            io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     
-        }
+        void Init();
 
         // Setup Platform/Renderer backends
-        static void AttachRenderer(GLFWwindow* window, const std::string& glslVersion)
-        {
-            ImGui_ImplGlfw_InitForOpenGL(window, true);
-            ImGui_ImplOpenGL3_Init(glslVersion.c_str());
-        }
+        void AttachRenderer(GLFWwindow* window, const std::string& glslVersion);
 
         // Set the style as either classic, dark, or light
-        static void SetStyle(ImguiStyle style)
-        {
-            switch (style)
-            {
-            case ImguiStyle::DARK:
-                ImGui::StyleColorsDark();
-                break;
-            case ImguiStyle::LIGHT:
-                ImGui::StyleColorsLight();
-                break;
-            case ImguiStyle::CLASSIC:
-                ImGui::StyleColorsClassic();
-                break;
-            default:
-                ImGui::StyleColorsDark();
-            }
-        }
+        void SetStyle(ImguiStyle style);
 
         // Begin the ImGUI frame
-        static void NewFrame()
-        {
-            // Start the Dear ImGui frame
-            ImGui_ImplOpenGL3_NewFrame();
-            ImGui_ImplGlfw_NewFrame();
-            ImGui::NewFrame();
-        }
+        void NewFrame();
 
         // End frame / Render all ImGUI draw data
-        static void Render()
-        {
-            ImGuiIO& io = ImGui::GetIO();
-
-            // Set the ImGUI display size to the current window width and height
-            Application* app = Application::get();
-            io.DisplaySize = ImVec2(app->getWindow()->getWidth(), app->getWindow()->getHeight());
-
-            ImGui::Render();
-            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        }
+        void Render();
 
         // Free all ImGUI resources 
-        static void Shutdown()
-        {
-            ImGui_ImplOpenGL3_Shutdown();
-            ImGui_ImplGlfw_Shutdown();
-            ImGui::DestroyContext();
-        }
+        void Shutdown();
 
         // Display ImGUI Demo Window
-        static void ShowDemoWindow(bool* show)
-        {
-            ImGui::ShowDemoWindow(show);
-        }
+        void ShowDemoWindow(bool* show);
 
 	} // namespace Gui
 
