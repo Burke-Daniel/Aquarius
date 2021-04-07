be2e6d2954b340d79b29e8efc14323688ea321d9
Colton Smith <cnsmith@mun.ca>
2021-04-07 04:48:08 -0230

    Add animation flag and speed in entity (makes the demo a little snazzier)

--- Sandbox/Entity.h
+++ Sandbox/Entity.h
@@ -6,18 +6,29 @@ class Entity
 {
 public:
 	Entity(const glm::vec2& pos,
-		   const glm::vec2& scale,
-		   const glm::vec4& color,
-		   float rot)	
-		:	m_position(pos),
-			m_scale(scale),
-			m_color(color),
-			m_rotation(rot) 
+		const glm::vec2& scale,
+		const glm::vec4& color,
+		float rot,
+		bool animate = false)
+		: m_position(pos),
+		m_scale(scale),
+		m_color(color),
+		m_rotation(rot),
+		m_isAnimated(animate),
+		m_animationSpeed(0)
 	{
 		m_id = id++;
 	}
 
 	~Entity() = default;
+
+	void onUpdate(Aquarius::timeDelta_t time)
+	{
+		if (m_isAnimated)
+		{
+			m_rotation = m_rotation > 360 ? 0 : m_rotation + time * m_animationSpeed;
+		}
+	}
 
 	void draw() const 
 	{
@@ -34,4 +45,6 @@ public:
 	glm::vec2 m_scale;
 	glm::vec4 m_color;
 	float m_rotation;
+	bool m_isAnimated;
+	float m_animationSpeed;
 };
